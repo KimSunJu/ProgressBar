08f7935150919be62ae687e9379c4c757f5252aa
Sunju Kim <sunju7420@naver.com>
2019-05-28 13:28:53 +0900

    진행상태바 코드구현

--- ProgressBar/Form1.Designer.cs
+++ ProgressBar/Form1.Designer.cs
@@ -94,6 +94,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "상태진행";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
